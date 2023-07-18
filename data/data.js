8a28e30cf48a7e0c8500025183d7089e72d3af31
amounach <mounach-@hotmail.com>
2023-07-18 15:30:09 +0100

    chore: add book data

---
+++ data/data.js
@@ -0,0 +1,59 @@
+const books = [
+
+{
+id: 1001,
+title: 'The Great Gatsby',
+author: 'F. Scott Fitzgerald',
+genre: 'Classic Fiction',
+publicationYear: 1925,
+isbn: '9780743273565',
+pageCount: 180,
+language: 'English'
+},
+
+{
+id: 1002,
+title: 'To Kill a Mockingbird',
+author: 'Harper Lee',
+genre: 'Novel',
+publicationYear: 1960,
+isbn: '9780061120084',
+pageCount: 281,
+language: 'English'
+},
+
+{
+id: 1003,
+title: '1984',
+author: 'George Orwell',
+genre: 'Dystopian Fiction',
+publicationYear: 1949,
+isbn: '9780451524935',
+pageCount: 328,
+language: 'English'
+},
+
+{
+id: 1004,
+title: 'Harry Potter and the Sorcerer',
+author: 'J.K. Rowling',
+genre: 'Fantasy',
+publicationYear: 1997,
+isbn: '9780590353427',
+pageCount: 320,
+language: 'English'
+},
+
+{
+id: 1005,
+title: 'The Hobbit',
+author: 'J.R.R. Tolkien',
+genre: 'Fantasy',
+publicationYear: 1937,
+isbn: '9780261102217',
+pageCount: 310,
+language: 'English'
+}
+
+]
+module.exports = books;
