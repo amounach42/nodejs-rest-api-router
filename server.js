2fff9a290096f9283ade36579c1a89c16606c00f
amounach <mounach-@hotmail.com>
2023-07-18 15:33:42 +0100

    chore: Add 'getBooks' function to retrieve all books using Book mode

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const http = require('http');
 const fs = require('fs');
+const books = require('./data/data');
+const { getBooks } = require('./controllers/bookController.js');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+
 const server = http.createServer((req, res) => {
+	if (req.url === '/api/books' && req.method === 'GET'){
+		getBooks(req, res)
+	}
+	else{
+		res.writeHead(404, { 'Content-type': 'application/json'});
+		res.end(JSON.stringify({message: 'Error: Route not found'}));
+	}
 	});
 
 
