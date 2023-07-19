d7669b2cce5957cc279cc4fedb250227718fc05b
amounach <mounach-@hotmail.com>
2023-07-19 13:45:42 +0100

    chore: Add function to get a single product by ID

--- models/bookModel.js
+++ models/bookModel.js
@@ -1,4 +1,6 @@
+const { resolve } = require('path');
 const books = require('../data/data');
+
 
 function findAll() {
 	return new Promise((resolve, reject) => {
@@ -6,6 +8,17 @@ function findAll() {
 	})
 }
 
+//@desc  Gets Single Product
+// @route GET /api/products/:id
+function findById(id) {
+	return new Promise((resolve, reject) => {
+		const book = books.find((b) => b.id  == id)
+		console.log("ID: ", id);
+		console.log("Book: ", book);
+		resolve(book)
+	})
+}
 module.exports = {
-	findAll
+	findAll,
+	findById
 }
